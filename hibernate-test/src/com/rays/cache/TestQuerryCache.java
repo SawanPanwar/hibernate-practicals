d3f319c690261bdb9e1d1b2901d4dd9463c1b8a5
SawanPanwar <skpanwar25@gmail.com>
2024-06-18 17:01:48 +0530

    done

--- hibernate-test/src/com/rays/cache/TestQuerryCache.java
+++ hibernate-test/src/com/rays/cache/TestQuerryCache.java
@@ -3,7 +3,6 @@ package com.rays.cache;
 import java.util.Iterator;
 import java.util.List;
 
-import org.hibernate.CacheMode;
 import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
