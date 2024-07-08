5126b64d6763a3094be3c2da79fbaa95624833c5
SawanPanwar <skpanwar25@gmail.com>
2024-07-08 11:11:25 +0530

    done

---
+++ hibernate-basics/src/com/rays/user/TestUser.java
@@ -0,0 +1,37 @@
+package com.rays.user;
+
+import java.util.Date;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class TestUser {
+
+	public static void main(String[] args) {
+
+		UserDTO dto = new UserDTO();
+
+		dto.setId(1);
+		dto.setFirstName("abc");
+		dto.setLastName("xyz");
+		dto.setLoginId("abc@gmail.com");
+		dto.setPassword("123");
+		dto.setDob(new Date());
+		dto.setAddress("indore");
+
+		SessionFactory sf = new Configuration().configure().buildSessionFactory();
+
+		Session session = sf.openSession();
+
+		Transaction tx = session.beginTransaction();
+
+		session.save(dto);
+
+		tx.commit();
+
+		session.close();
+
+	}
+}
